c8bd5d92451053c263afc4ec58edc5d35b5e673e
Tiago Augusto Santos Goes <tiagoasg@examroom.ai>
2026-03-06 15:34:36 -0300

    bugfix

--- backend/IdentityService/API/IdentityService.API/Program.cs
+++ backend/IdentityService/API/IdentityService.API/Program.cs
@@ -6,6 +6,9 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- backend/IdentityService/Domain/Interfaces/IUserRepository.cs
+++
@@ -1,8 +0,0 @@
-namespace IdentityService.Domain.Interfaces;
-
-public interface IUserRepository
-{
-    Task<User?> GetByEmailAsync(string email);
-    Task AddAsync(User user);
-    Task<bool> ExistsAsync(string email);
-}
